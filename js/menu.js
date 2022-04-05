54c54aed64d5c14a9e871a4f3579555f3e2f05b2
Lanterna IT <admin@lit.hr>
2022-04-05 13:41:43 +0200

    mobile_version_scroll_type

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,24 @@
-var menuBars = document.querySelector(".fa-bars");
+var menuBars = document.querySelector(".menu-bars");
+var menuBarsClose = document.querySelector(".menu-bars-close");
 var menuMobile = document.querySelector(".menu-mobile");
 
 menuBars.addEventListener("click", function () {
-  if (menuMobile.style.display === "block") menuMobile.style.display = "none";
-  else {
+  if (menuMobile.style.display === "block") {
+    menuMobile.style.display = "none";
+  } else {
     menuMobile.style.display = "block";
+    menuBarsClose.style.display = "block";
+    menuBars.style.display = "none";
+    menuBarsClose.addEventListener("click", () => {
+      if (menuMobile.style.display === "block") {
+        menuMobile.style.display = "none";
+        menuBars.style.display = "block";
+        menuBarsClose.style.display = "none";
+      } else {
+        menuBars.style.display = "block";
+        menuBarsClose.style.display = "none";
+      }
+    });
+    // menuBars.style.display = "none";
   }
 });
